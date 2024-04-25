1c66618c2fd1227e8641a6f886696932c60a1702
bg <bg@GWNR71517.localdomain>
2024-04-25 17:25:51 -0300

    segundo commit

---
+++ LabSD/experimento-5/exp5_q1.vhd
@@ -0,0 +1,57 @@
+-- Universidade de Brasília
+-- Laboratório de Sistemas Digitais
+-- Autor : Bernardo Gomes Rodrigues 
+-- Matrícula: 231034190
+-- Data : 25/04/2024
+
+-- Implementação de um somador de palavras de 4 bits utilizando somadores completos
+
+-- ****************************************** 
+--  Circuito: Somador de palavras de 4 bits:
+--                A Entrada 1
+--                B Entrada 2
+--                S Saida
+-- ******************************************
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity exp5_q1 is
+
+    port (
+        A : in std_logic_vector(3 downto 0);
+        B : in std_logic_vector(3 downto 0);
+        S : out std_logic_vector(4 downto 0)
+    );
+
+end exp5_q1;
+
+architecture Behavioral of exp5-q1 is
+
+    component SOMADOR is
+
+        port (
+            A : in std_logic;
+            B : in std_logic;
+            Cin : in std_logic;
+            S : out std_logic;
+            Cout : out std_logic
+        );
+    end component;
+
+signal carry : in std_logic_vector(4 downto 0);
+
+begin
+
+    carry(0) <= '0';
+
+    for i in 0 to 3 generate
+        somador_inst: SOMADOR port map (A(i), B(i), carry(i), S(i), carry(i+1));
+    end generate;
+
+    S(4) <= carry(4);
+
+    end Behavioral;
+
+
---
+++ LabSD/experimento-5/somador.vhd
@@ -0,0 +1,38 @@
+-- Universidade de Brasília
+-- Laboratório de Sistemas Digitais
+-- Autor : Bernardo Gomes Rodrigues
+-- Data : 25/04/2024
+
+-- Implementação de um Somador Completo com 3 bits de entrada e 2 de saída
+
+-- ****************************************** 
+--  Circuito: Somador de 3 entradas: 
+--                A Entrada 1
+--                B Entrada 2
+--                Cin Entrada 3
+--                S Saida 1
+--                Cout  Saida2
+-- ******************************************
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity SOMADOR 
+    is port (
+        A      :in std_logic; 
+        B      :in std_logic; 
+        Cin    :in std_logic; 
+        S      :out std_logic; 
+        Cout   :out std_logic 
+    );
+end SOMADOR;
+
+architecture Behavioral of SOMADOR is
+
+begin
+
+S <= A xor B xor Cin;
+
+Cout <= (A and B) or (A and Cin) or (B and Cin);
+
+end Behavioral;
