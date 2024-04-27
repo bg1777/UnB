31bb70322a7cab4249adc5cfacc9d9fc66cd6e9b
bg <bg@GWNR71517.localdomain>
2024-04-27 18:28:15 -0300

    yay

--- MP/MP - Trabalho 1/romanos.cpp
+++ MP/MP - Trabalho 1/romanos.cpp
@@ -1,6 +1,116 @@
 #include "romanos.hpp"
-int romanos_para_decimal(char const * num_romano)
-{
-  return 0; 
+
+bool mais_de_quatro_repeticoes(const char *num_romano) {
+    int frequencia[7] = {0};
+    for (int i = 0; num_romano[i] != '\0'; i++) {
+        char caractere = num_romano[i];
+        switch (caractere) {
+            case 'I':
+                frequencia[0]++;
+                break;
+            case 'V':
+                frequencia[1]++;
+                break;
+            case 'X':
+                frequencia[2]++;
+                break;
+            case 'L':
+                frequencia[3]++;
+                break;
+            case 'C':
+                frequencia[4]++;
+                break;
+            case 'D':
+                frequencia[5]++;
+                break;
+            case 'M':
+                frequencia[6]++;
+                break;
+        }
+    }
+
+    for (int i = 0; i < 7; i++) {
+        if (frequencia[i] > 4) {
+            return true;
+        }
+    }
+
+    return false;
 }
- 
+
+bool tem_repeticao_consecutiva(const char *num_romano) {
+    int contador = 1;
+    char caractere_anterior = num_romano[0];
+    for (int i = 1; num_romano[i] != '\0'; i++) {
+        if (num_romano[i] == caractere_anterior) {
+            contador++;
+            if (contador > 3) {
+                return true;
+            }
+        } else {
+            contador = 1;
+            caractere_anterior = num_romano[i];
+        }
+    }
+    return false;
+}
+
+int valor_romano(char caractere) {
+    switch (caractere) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return -1;
+    }
+}
+
+int romanos_para_decimal(char const * num_romano) {
+    if (mais_de_quatro_repeticoes(num_romano)) {
+        return -1;
+    }
+
+    if (tem_repeticao_consecutiva(num_romano)) {
+        return -1;
+    }
+
+    int decimal = 0;
+
+    if (num_romano == nullptr || *num_romano == '\0') {
+        return -1;
+    }
+
+    for (int i = 0; num_romano[i] != '\0'; i++) {
+        int valor_atual = valor_romano(num_romano[i]);
+        if (valor_atual == -1) {
+            return -1;
+        }
+
+        if (num_romano[i + 1] != '\0' && valor_atual < valor_romano(num_romano[i + 1])) {
+            if (i > 0 && valor_romano(num_romano[i - 1]) <= valor_atual) {
+                return -1;
+            }
+            int subtracao = valor_romano(num_romano[i + 1]) - valor_atual;
+            if (subtracao == 4 || subtracao == 5 || subtracao == 40 || subtracao == 50 || subtracao == 400 || subtracao == 500) {
+                decimal -= valor_atual;
+            } else {
+                return -1;
+            }
+        } else {
+            decimal += valor_atual;
+        }
+    }
+
+    return decimal;
+}
--- MP/MP - Trabalho 1/testa_romanos.cpp
+++ MP/MP - Trabalho 1/testa_romanos.cpp
@@ -1,24 +1,35 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "romanos.hpp"
 
-
-TEST_CASE( "Numeros romanos - algarismos únicos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("I") == 1 );
-    
-    REQUIRE( romanos_para_decimal("V") == 5 );
-
-    
+TEST_CASE("Numeros romanos - algarismos Ãºnicos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("I") == 1);
+    REQUIRE(romanos_para_decimal("V") == 5);
+    REQUIRE(romanos_para_decimal("X") == 10);
+    REQUIRE(romanos_para_decimal("L") == 50);
+    REQUIRE(romanos_para_decimal("C") == 100);
+    REQUIRE(romanos_para_decimal("D") == 500);
+    REQUIRE(romanos_para_decimal("M") == 1000);
 }
 
-
-
-
-TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("G") == -1 );
-    
-    REQUIRE( romanos_para_decimal("i") == -1 );
-
-    
+TEST_CASE("Numeros romanos - NÃºmeros compostos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("II") == 2);
+    REQUIRE(romanos_para_decimal("III") == 3);
+    REQUIRE(romanos_para_decimal("IIII") == -1);
+    REQUIRE(romanos_para_decimal("CCXXXI") == 231);
+    REQUIRE(romanos_para_decimal("LXV") == 65);
+    REQUIRE(romanos_para_decimal("XXX") == 30);
+    REQUIRE(romanos_para_decimal("CCXXXI") == 231);
+    REQUIRE(romanos_para_decimal("CCXXXIXXXLLL") == -1);
+    REQUIRE(romanos_para_decimal("IV") == 4);
+    REQUIRE(romanos_para_decimal("IVXXXXXXXXXXXXXXX") == -1);
+    REQUIRE(romanos_para_decimal("XXXLL") == -1);
+    REQUIRE(romanos_para_decimal("IM") == -1);
+    REQUIRE(romanos_para_decimal("CDXXXVI") == 436);
 }
- 
+
+TEST_CASE("Numeros romanos - algarismos invÃ¡lidos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("G") == -1);
+    REQUIRE(romanos_para_decimal("i") == -1);
+    REQUIRE(romanos_para_decimal("VIB") == -1);
+}
